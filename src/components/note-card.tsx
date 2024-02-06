1ac406aad3044341921bffed8e763f2adacd6567
jhuly ambrozini <jhulyamfebr@gmial.com>
2024-02-06 16:18:25 -0300

    feat: criando homepage

---
+++ src/components/note-card.tsx
@@ -0,0 +1,12 @@
+export function NoteCard() {
+  return (
+    <button className="rounded-md bg-slate-800 p-5 space-y-6 overflow-hidden relative hover:ring-2 hover:ring-slate-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+      <p className="text-sm leading-6 text-slate-400">
+        Grave uma nota em áudio que será convertida para texto automaticamente.
+      </p>
+
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+    </button>
+  );
+}
